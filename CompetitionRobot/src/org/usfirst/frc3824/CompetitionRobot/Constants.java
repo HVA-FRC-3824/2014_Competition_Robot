8198ebb24b61d8ef45d5503665a1fab5d70e4be6
Spencer Gehin <scGehin@gmail.com>
2014-02-01 14:20:05 -0500

    Copied constants from last years robot code and translated them to java.

--- CompetitionRobot/src/org/usfirst/frc3824/CompetitionRobot/Constants.java
+++ CompetitionRobot/src/org/usfirst/frc3824/CompetitionRobot/Constants.java
@@ -14,4 +14,65 @@ public class Constants
     
     public final static double SHOOTER_ANGLE_MAX_VALUE =  850;
     public final static double SHOOTER_ANGLE_MIN_VALUE =  0;
+
+    /* Constants copied from last years robot *//****** Shooter angle conversion (ADC to angle) ******/
+    /* angle = (SLOPE * ADC) + Y_INTERCEPT   */
+    /* ADC   = (angle - Y_INTERCEPT) / SLOPE */
+    public final static double SHOOTER_ANGLE_SLOPE = 0.0337;
+    public final static double SHOOTER_ANGLE_Y_INTERCEPT = 17.203;
+    /****** BACK/UNDER TOWER ******/
+    public final static double SHOOTER_ANGLE_BACK_OF_TOWER = 29.5 ;
+    public final static double SHOOTER_ANGLE_UNDER_TOWER = 30.25 ;
+    public final static double SHOOTER_ANGLE_FOR_PICKUP = 21.584;
+    public final static double SHOOTER_SPEED_BACK_OF_TOWER = 3200.0;
+    /****** SHOOTER WAIT TIMES ******/
+    public final static double AUTONOMOUS_SHOOT_WAIT1 = 0.0;
+    public final static double AUTONOMOUS_SHOOT_WAIT2 = 0.0;
+    // Loadind define
+    public final static double POSITION_LOADING = 36.0;
+    public final static double POSITION_INITIALIZE = 34.3; //28.0
+    /****** SHOOTER POSITION ******/
+    // Note: The shooter angle is greater than possible to ensure the new shooter 
+    //       can move the full range (after the slope and y-intercept have been 
+    //       recalibrated)
+    public final static double SHOOTER_ANGLE_THRESHOLD = 2.0;    // Note: in ADC values
+    public final static double SHOOTER_ANGLE_MAX_PICKUP_VALUE = 23.943;  //  200.0
+    public final static double SHOOTER_ANGLE_PICKUP_VALUE = 21.584;  //  130.0
+    /****** SHOOTER SPEED ******/
+    public final static double SHOOTER_SPEED_THRESHOLD = 50.0;
+    public final static double SHOOTER_SPEED_MAX_VALUE = 4000.0;
+    public final static double SHOOTER_SPEED_MIN_VALUE = 1000.0;
+    /****** PICKUP VOLTAGE ******/
+    public final static double PICKUP_UP_VOLTAGE = -0.5;
+    public final static double PICKUP_DOWN_VOLTAGE = 0.4;
+    /****** CYPRESS POSITION DEFINES ******/
+    // front of pyramid
+    public final static double POSITION_1_ANGLE = 40.793;  // 700.0
+    public final static double POSITION_1_VELOCITY = 2500.0;
+    public final static double POSITION_1_VOLTAGE = 0.6;
+    public final static boolean POSITION_1_IS_RPM = true;
+    // back of pyramid
+    public final static double POSITION_2_ANGLE = 28.0;  // 230.0 
+    public final static double POSITION_2_VELOCITY = 3200.0;
+    public final static double POSITION_2_VOLTAGE = 0.62;
+    public final static boolean POSITION_2_IS_RPM = true;
+    // under the tower and loading
+    public final static double POSITION_3_ANGLE = SHOOTER_ANGLE_BACK_OF_TOWER ;
+    public final static double POSITION_3_VELOCITY = SHOOTER_SPEED_BACK_OF_TOWER;
+    public final static double POSITION_3_VOLTAGE = 0.6;
+    public final static boolean POSITION_3_IS_RPM = true;
+    // Long Shoot
+    public final static double POSITION_4_ANGLE = 10.0; // 854.0 
+    public final static double POSITION_4_VELOCITY = 3000.0;
+    public final static double POSITION_4_VOLTAGE = 0.6;
+    public final static boolean POSITION_4_IS_RPM = true;
+    // loading position
+    public final static double POSITION_5_ANGLE = POSITION_LOADING ;
+    public final static double POSITION_5_VELOCITY = SHOOTER_SPEED_BACK_OF_TOWER;
+    public final static double POSITION_5_VOLTAGE = 0.6;
+    public final static boolean POSITION_5_IS_RPM = true;
+    // shooting when handing on the tower
+    public final static double HANGING_ON_TOWER_ANGLE = 28.5;
+    public final static double HANGING_ON_TOWER_VELOCITY = 3600.0;
+    public final static boolean HANGING_ON_TOWER_IS_RPM = true;
 }
