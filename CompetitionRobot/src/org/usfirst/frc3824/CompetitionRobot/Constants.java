a6b57be56e99244efc9c3544ea57eb7175b3e393
Mike Goin <mike.goin12@gmail.com>
2014-02-01 11:18:28 -0500

    Merged with PromoChassis code

---
+++ CompetitionRobot/src/org/usfirst/frc3824/CompetitionRobot/Constants.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc3824.CompetitionRobot;
+
+/**
+ * Put any important constants here.
+ */
+public class Constants
+{
+    /* Contants which might require tuning */
+    public final static double DrivetrainAngleGyroControllerP = .1;
+    public final static double DrivetrainAngleGyroControllerI = .01;
+    public final static double DrivetrainAngleGyroControllerD = .01;
+}
