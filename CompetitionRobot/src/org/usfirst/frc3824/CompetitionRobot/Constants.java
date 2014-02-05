c258c14bd9df605489f2faf9c912d2d6600ce636
Mike Goin <mike.goin12@gmail.com>
2014-02-04 20:02:39 -0500

    Added and changed around Constants

--- CompetitionRobot/src/org/usfirst/frc3824/CompetitionRobot/Constants.java
+++ CompetitionRobot/src/org/usfirst/frc3824/CompetitionRobot/Constants.java
@@ -12,10 +12,18 @@ public class Constants
     
     public final static int ANALOG_SHOOTER_ADJUST_PID = 3; // pot input channel
     
-    public final static double SHOOTER_ANGLE_MAX_VALUE =  850;
-    public final static double SHOOTER_ANGLE_MIN_VALUE =  0;
+    /****** SHOOTER POSITION ******/
+    // Note: The shooter angle is greater than possible to ensure the new shooter 
+    //       can move the full range (after the slope and y-intercept have been 
+    //       recalibrated)
+    public final static double SHOOTER_ANGLE_MAX_VALUE = 50.0;    // 850.0  - 45.848
+    public final static double SHOOTER_ANGLE_MIN_VALUE = 10.0;    //   0.0  - 17.203
+    public final static double SHOOTER_ANGLE_THRESHOLD = 2.0;    // Note: in ADC values
+    public final static double SHOOTER_ANGLE_MAX_PICKUP_VALUE = 23.943;  //  200.0
+    public final static double SHOOTER_ANGLE_PICKUP_VALUE = 21.584;  //  130.0
 
-    /* Constants copied from last years robot *//****** Shooter angle conversion (ADC to angle) ******/
+    /* Constants copied from last years robot */
+    /****** Shooter angle conversion (ADC to angle) ******/
     /* angle = (SLOPE * ADC) + Y_INTERCEPT   */
     /* ADC   = (angle - Y_INTERCEPT) / SLOPE */
     public final static double SHOOTER_ANGLE_SLOPE = 0.0337;
@@ -31,20 +39,13 @@ public class Constants
     // Loadind define
     public final static double POSITION_LOADING = 36.0;
     public final static double POSITION_INITIALIZE = 34.3; //28.0
-    /****** SHOOTER POSITION ******/
-    // Note: The shooter angle is greater than possible to ensure the new shooter 
-    //       can move the full range (after the slope and y-intercept have been 
-    //       recalibrated)
-    public final static double SHOOTER_ANGLE_THRESHOLD = 2.0;    // Note: in ADC values
-    public final static double SHOOTER_ANGLE_MAX_PICKUP_VALUE = 23.943;  //  200.0
-    public final static double SHOOTER_ANGLE_PICKUP_VALUE = 21.584;  //  130.0
     /****** SHOOTER SPEED ******/
     public final static double SHOOTER_SPEED_THRESHOLD = 50.0;
     public final static double SHOOTER_SPEED_MAX_VALUE = 4000.0;
     public final static double SHOOTER_SPEED_MIN_VALUE = 1000.0;
     /****** PICKUP VOLTAGE ******/
-    public final static double PICKUP_UP_VOLTAGE = -0.5;
-    public final static double PICKUP_DOWN_VOLTAGE = 0.4;
+    public final static double PICKUP_OUT_VOLTAGE = -0.5;
+    public final static double PICKUP_IN_VOLTAGE = 0.4;
     /****** CYPRESS POSITION DEFINES ******/
     // front of pyramid
     public final static double POSITION_1_ANGLE = 40.793;  // 700.0
